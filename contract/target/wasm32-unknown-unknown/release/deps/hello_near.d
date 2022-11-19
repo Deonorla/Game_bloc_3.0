1c5da84c61302103dc2f8ea39ba1de3e5ac96e67
marvincd <61430224+marvincd@users.noreply.github.com>
2022-11-19 17:48:25 +0100

    updates for the functions

--- contract/target/wasm32-unknown-unknown/release/deps/hello_near.d
+++ contract/target/wasm32-unknown-unknown/release/deps/hello_near.d
@@ -1,5 +1,5 @@
-/root/Github/Game_bloc/contract/target/wasm32-unknown-unknown/release/deps/hello_near.wasm: src/lib.rs
+/Users/pro/Documents/GitHub/Game_block/contract/target/wasm32-unknown-unknown/release/deps/hello_near.wasm: src/lib.rs
 
-/root/Github/Game_bloc/contract/target/wasm32-unknown-unknown/release/deps/hello_near.d: src/lib.rs
+/Users/pro/Documents/GitHub/Game_block/contract/target/wasm32-unknown-unknown/release/deps/hello_near.d: src/lib.rs
 
 src/lib.rs:
--- contract/target/wasm32-unknown-unknown/release/hello_near.d
+++ contract/target/wasm32-unknown-unknown/release/hello_near.d
@@ -1,1 +1,1 @@
-/root/Github/Game_bloc/contract/target/wasm32-unknown-unknown/release/hello_near.wasm: /root/Github/Game_bloc/contract/src/lib.rs
+/Users/pro/Documents/GitHub/Game_block/contract/target/wasm32-unknown-unknown/release/hello_near.wasm: /Users/pro/Documents/GitHub/Game_block/contract/src/lib.rs
