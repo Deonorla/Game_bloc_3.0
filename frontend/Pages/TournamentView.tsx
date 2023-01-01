3fae9b342c4913ff6b71f673b0e785fdcea064a2
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-01-01 19:07:24 +0100

    tournament-view design 80%

--- frontend/Pages/TournamentView.tsx
+++ frontend/Pages/TournamentView.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
 import check from "../assets/check.png";
+import dollar from "../assets/dollar.png";
+import money from "../assets/money.png";
+import mode from "../assets/mode.png";
 
 const TournamentView = () => {
   return (
@@ -22,17 +25,64 @@ const TournamentView = () => {
               </AvatarContainer>
           </GameImg>
 
+          <GameMode>
+             <ModeContainer>
+                <img src={mode} alt="" />
+               <div>
+                  <p>Game Mode</p>
+                  <p>$20</p>
+               </div>
+             </ModeContainer>
+
+             <ModeContainer>
+                <img src={money} alt="" />
+                <div>
+                  <p>Pool prize </p>
+                  <p>$20</p>
+                </div>
+             </ModeContainer>
+
+             <ModeContainer>
+                <img src={dollar} alt="" />
+                <div>
+                 <p> Entry prize </p>
+                  <p>$20</p>
+                </div>
+             </ModeContainer>
+
+              <Button>
+                   Join
+              </Button>
+          </GameMode>
+
+          <About>
+             <Heading>About</Heading>
+
+              <Tags>
+                <Tag1>Shooting</Tag1>
+                <Tag2>Adventure</Tag2>
+                <Tag3>Action</Tag3>
+              </Tags>
+
+              <Info>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+               dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+               sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Info>
+
+          </About>
+
     </Container>
   )
 }
 
 const Container = styled.div`
 margin-top: 4.5rem;
-  width: 100%;
+  width: 95%;
   flex-grow: 1;
   flex-shrink: 1;
-  border-top-right-radius: 12px;
-  border-top-left-radius: 12px;
+  border-radius: 12px;
   @media (min-width: 1200px) {
     width: 60vw;
 
@@ -40,8 +90,7 @@ margin-top: 4.5rem;
 `;
 
 const GameImg = styled.div`
-  border-top-right-radius: 12px;
-  border-top-left-radius: 12px;
+  border-radius: 12px;
   height: 25rem;
    background-color: #35356b;
 `;
@@ -104,5 +153,142 @@ const UserName = styled.div`
 
 const SideDetails = styled.div`
 `;
+
+const GameMode = styled.div`
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ background-color: #35356b;
+ margin-top: 2rem;
+ border-radius: 8px;
+ padding: 1rem;
+ width: 96%;
+
+`;
+
+const ModeContainer =styled.div`
+  display: flex;
+     flex-direction: row;
+     justify-content: space-between;
+     align-items: center;
+     margin-left: 2rem;
+   div{
+      display: flex;
+      flex-direction: column;
+      p{
+           color: white;
+           font-size: .8rem;
+           font-weight: 500;
+           margin: .5rem 0 0 1rem;
+         }
+     }
+
+   img{
+    width: 40px;
+    height: 40px;
+   }
+
+`;
+
+const Button = styled.button`
+   background-color: #41417c;
+   border-radius: 8px;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   padding: 1rem 2rem;
+   color: white;
+   font-size: 16px;
+   border: none;
+   width: fit-content;
+   cursor: pointer;
+   &:hover{
+      scale: 1.03;
+      background-color: #6365bf;
+   }
+`;
+
+const About = styled.div`
+ background-color: #35356b;
+ border-radius: 12px;
+ width: 96%;
+ padding: 1rem;
+ margin-top: 2rem;
+`;
+
+const Heading = styled.h4`
+ color: white;
+ font-size: 1.5rem;
+ margin: 0;
+`;
+
+const Tags = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Tag1 = styled.div`
+  display: flex;
+  color: white;
+  flex-direction: row;
+  align-items: center;
+  align-self: flex-end;
+  background: #991ba5;
+  border-radius: 9999px;
+  padding: 0.3rem 0.7rem;
+  width: fit-content;
+  margin: 1rem;
+  cursor: pointer;
+  p {
+    color: #fff;
+    font-size: 1rem;
+    margin: 0 0 0 8px;
+  }
+`;
+
+const Tag2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-self: flex-end;
+  background: #057faf;
+  border-radius: 9999px;
+  padding: 0.3rem 0.7rem;
+  width: fit-content;
+  margin: 1rem;
+  cursor: pointer;
+  color: white;
+  p {
+    color: #fff;
+    font-size: 1rem;
+    margin: 0 0 0 8px;
+  }
+`;
+
+const Tag3 = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-self: flex-end;
+  background: #05af93;
+  border-radius: 9999px;
+  padding: 0.3rem 0.7rem;
+  width: fit-content;
+  margin: 1rem;
+  color: white;
+  cursor: pointer;
+  p {
+    color: #fff;
+    font-size: 1rem;
+    margin: 0 0 0 8px;
+  }
+`;
+
+const Info = styled.p`
+ margin-left: 1rem;
+ color: white;
+ opacity: 0.7;
+`;
+
 
 export default TournamentView;
