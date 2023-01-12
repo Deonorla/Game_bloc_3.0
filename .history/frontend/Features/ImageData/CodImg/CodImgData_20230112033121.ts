87e2931fb636c89274d890c9092fc314c33259dd
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2023-01-12 04:35:37 +0100

    added more images to tournament cards

---
+++ .history/frontend/Features/ImageData/CodImg/CodImgData_20230112033121.ts
@@ -0,0 +1,43 @@
+const CodImgData = [
+
+         "https://images2.alphacoders.com/617/thumbbig-617634.webp"
+    ,
+    
+    
+         "https://images5.alphacoders.com/617/thumbbig-617630.webp"
+    ,
+    
+        
+         "https://images6.alphacoders.com/330/thumbbig-330739.webp"
+    ,
+    
+        
+         "https://images5.alphacoders.com/314/thumbbig-314689.webp"
+    ,
+    
+        
+         "https://images3.alphacoders.com/102/thumbbig-102917.webp"
+    ,
+    
+        
+         "https://images8.alphacoders.com/484/thumbbig-484935.webp"
+    ,
+    
+        "https://images5.alphacoders.com/336/thumbbig-336250.webp"
+    ,
+    
+    
+         "https://images2.alphacoders.com/448/thumbbig-448077.webp"
+    ,
+    
+    
+         "https://images3.alphacoders.com/101/thumbbig-1018513.webp"
+    ,
+    
+        
+         "https://images3.alphacoders.com/978/thumbbig-978554.webp"
+    
+     
+]
+ 
+export default CodImgData;
